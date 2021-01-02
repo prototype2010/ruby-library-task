436088260889f768e78e0e202c37a654f20c1235
prototype2010 <Firstday2010>
2021-01-02 20:50:15 +0200

    working without db file

--- lib_loader/lib_loader.rb
+++ lib_loader/lib_loader.rb
@@ -1,23 +1,27 @@
 require 'json'
 
 class LibLoader
-  def self.load(file_name = './lib.json')
-    json = read_lib_file(file_name)
+  DEFAULT_FILENAME = './lib.json'.freeze
+
+  def self.load(file_name = DEFAULT_FILENAME)
+    json = File.read(file_name)
     parse_json_file(json)
+  rescue StandardError => e
+    puts "Unable to get lib content from #{file_name}. Empty library will be instantiated"
+    puts "Original error text: #{e.message}"
+    {
+      authors: [],
+      books: [],
+      orders: [],
+      readers: []
+    }
   end
 
   def self.parse_json_file(lib_content)
     JSON.parse(lib_content, { object_class: Hash, symbolize_names: true })
   end
 
-  def self.write(lib_content, file_name = './lib.json')
+  def self.write(lib_content, file_name = DEFAULT_FILENAME)
     File.write(file_name, lib_content)
-  end
-
-  def self.read_lib_file(file_path)
-    File.read(file_path)
-  rescue StandardError => e
-    puts "Unable to get content from file by path #{file_path}. Make sure file exists and contains valid JSON"
-    raise e
   end
 end
