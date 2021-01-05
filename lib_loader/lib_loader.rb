fddc9163c7a7fc95691387cf2ca547f9c076c408
prototype2010 <Firstday2010>
2021-01-05 22:31:31 +0200

    no puts

--- lib_loader/lib_loader.rb
+++ lib_loader/lib_loader.rb
@@ -6,16 +6,15 @@ module LibLoader
   DEFAULT_FILENAME = File.expand_path('./lib.json')
 
   def load(file_name = DEFAULT_FILENAME)
-    json = File.read(file_name)
-    parsed = parse_json_file(json)
-
-    DependencyResolver.resolve(DEPENDENCY_RESOLVE_ORDER, DEPENDENCY_RESOLVE_CONFIG, parsed)
-  rescue StandardError => e
-    puts "Unable to get lib content from #{file_name}. Empty library will be instantiated"
-    puts "Original error text: #{e.message}, #{e.backtrace}"
-    {
-      authors: [], books: [], orders: [], readers: []
-    }
+    if File.exist? file_name
+      json = File.read(file_name)
+      parsed = parse_json_file(json)
+      DependencyResolver.resolve(DEPENDENCY_RESOLVE_ORDER, DEPENDENCY_RESOLVE_CONFIG, parsed)
+    else
+      {
+        authors: [], books: [], orders: [], readers: []
+      }
+    end
   end
 
   def parse_json_file(lib_content)
