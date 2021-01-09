02d2a814631db950c5a6ace9b53039a21724f5ad
prototype2010 <Firstday2010>
2021-01-09 20:12:14 +0200

    variable names

--- dependency_resolver/dependency_resolver.rb
+++ dependency_resolver/dependency_resolver.rb
@@ -1,33 +1,33 @@
 class DependencyResolver
-  def self.resolve(resolve_order_array, factories_hash, library_hash)
-    resolve_order_array.each_with_object({}) do |resolve_config, library_content|
+  def self.resolve(resolve_order_config, factories_config, json_file_content)
+    resolve_order_config.each_with_object({}) do |resolve_config, library_init_params|
       entities_key, factory = resolve_config
 
-      json_array = library_hash[entities_key]
+      json_array = json_file_content[entities_key]
 
-      library_content[entities_key] = resolve_group_deps(factory, json_array, factories_hash)
+      library_init_params[entities_key] = resolve_array_deps(factory, json_array, factories_config)
                                       .uniq
     end
   end
 
-  def self.resolve_group_deps(entity_factory, json_rows_array, factories_hash)
+  def self.resolve_array_deps(entity_factory, json_rows_array, factories_config)
     json_rows_array.map do |init_params|
-      external_dependencies = resolve_external_deps(init_params, factories_hash)
+      external_dependencies = resolve_one_deps(init_params, factories_config)
 
       entity_factory.create(init_params.merge(external_dependencies))
     end
   end
 
-  def self.resolve_external_deps(init_params, factories_hash)
+  def self.resolve_one_deps(init_params, factories_config)
     init_params
       .to_a
       .each_with_object({}) do |entry, resolved_deps|
       sym_key, entity_id = entry
 
-      next unless factories_hash.key?(sym_key)
+      next unless factories_config.key?(sym_key)
 
-      required_factory = factories_hash[sym_key][:factory]
-      initialize_prop = factories_hash[sym_key][:property_name]
+      required_factory = factories_config[sym_key][:factory]
+      initialize_prop = factories_config[sym_key][:property_name]
       resolved_deps[initialize_prop] = required_factory.find_by_id(entity_id)
     end
   end
